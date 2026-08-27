cff633afd006d0505651901a8bf6993a01103938
Roosevelt Silva <roosilva@gmail.com>
2026-08-27 18:13:28 -0400

    Subindo o projeto BootDrive

---
+++ src/main/java/br/edu/ifms/frameworks/bootdrive/BootdriveApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.frameworks.bootdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifms/frameworks/bootdrive/BootdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.frameworks.bootdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

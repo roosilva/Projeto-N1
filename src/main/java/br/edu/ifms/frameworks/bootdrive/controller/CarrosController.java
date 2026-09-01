fc68748f184e68cf9147c6c568bb9637bd1f489d
Roosevelt Silva <roosilva@gmail.com>
2026-09-01 19:52:20 -0400

    Aula 02

--- src/main/java/br/edu/ifms/frameworks/bootdrive/controller/CarrosController.java
+++ src/main/java/br/edu/ifms/frameworks/bootdrive/controller/CarrosController.java
@@ -1,14 +1,42 @@
 package br.edu.ifms.frameworks.bootdrive.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifms.frameworks.bootdrive.model.Carro;
 
 @Controller
 public class CarrosController {
     
     @GetMapping("/carros")
-    public String listar() {
-        return "ListaCarros";
+    public ModelAndView listarCarros() {
+        ModelAndView modelAndView = new ModelAndView("ListaCarros");
+        modelAndView.addObject("carros", buscarCarros());
+        return modelAndView;
     }
+
+    public List<Carro> buscarCarros() {
+        List<Carro> carros = new ArrayList<Carro>();
+        Carro c1 = new Carro();
+        Carro c2 = new Carro();
+        Carro c3 = new Carro();
+        c1.setId((long) 1);
+        c1.setPlaca("HSR-2345");
+        c2.setId((long) 2);
+        c2.setPlaca("OOR-7463");
+        c3.setId((long) 3);
+        c3.setPlaca("KNA-1098");
+        carros.add(c1);
+        carros.add(c2);
+        carros.add(c3);
+        return carros;
+    }
+
+
+    
 
 }
